085e1b713a6258697ac72f7d7fbd28e578dc845a
mbbssag3 <mbbssag3@spinnaker-HP-EliteDesk-800-G1-SFF>
2017-05-11 14:27:49 +0100

    Edit setup.py with correct requirements

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="1!4.0.0a5",
     packages=['python_models8',],
     package_data={'python_models8.model_binaries': ['*.aplx']},
-    install_requires=['SpyNNaker >= 1!4.0.0a5, < 1!5.0.0']
+    install_requires=['sPyNNaker8 >= 1!4.0.0a5, < 1!5.0.0']
 )
