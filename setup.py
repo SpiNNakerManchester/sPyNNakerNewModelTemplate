033094ab3fa658e0e8c3fffc4125b0ed170d4a8a
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-05-11 11:40:09 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sPyNNaker8NewModelTemplate",
-    version="1.0.0",
+    version="1!4.0.0a5",
     packages=['python_models8',],
     package_data={'python_models8.model_binaries': ['*.aplx']},
     install_requires=['SpyNNaker >= 1!4.0.0a5, < 1!5.0.0']
