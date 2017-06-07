23571018384726c14e04bd181b6287fc9727b50a
Andrew Gait <andrew.gait@manchester.ac.uk>
2017-06-07 15:24:28 +0100

    Add correct method to search for binaries
    - noticed that this is what is being used by ExtraModelsPlugin,
    so it should be used here too

--- python_models8/__init__.py
+++ python_models8/__init__.py
@@ -4,6 +4,5 @@ from python_models8 import model_binaries
 import os
 
 # This adds the model binaries path to the paths searched by sPyNNaker
-# Seems quite an ugly way of doing it, though... ?
-executable_finder = AbstractSpiNNakerCommon._EXECUTABLE_FINDER
-executable_finder.add_path(os.path.dirname(model_binaries.__file__))
+AbstractSpiNNakerCommon.register_binary_search_path(
+    os.path.dirname(model_binaries.__file__))
