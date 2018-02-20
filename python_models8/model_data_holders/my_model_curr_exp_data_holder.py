5dd60fb683ed79ce46ceddde2d9b3c7ad060a29e
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2018-02-20 12:38:14 +0100

    renamed none_pynn_default_parameters to non_pynn_default_parameters

--- python_models8/model_data_holders/my_model_curr_exp_data_holder.py
+++ python_models8/model_data_holders/my_model_curr_exp_data_holder.py
@@ -8,17 +8,17 @@ from python_models8.neuron.builds.my_model_curr_exp import MyModelCurrExpBase
 class MyModelCurrExpDataHolder(DataHolder):
     def __init__(
             self, spikes_per_second=(
-                AbstractPopulationVertex.none_pynn_default_parameters[
+                AbstractPopulationVertex.non_pynn_default_parameters[
                     'spikes_per_second']),
             ring_buffer_sigma=(
-                AbstractPopulationVertex.none_pynn_default_parameters[
+                AbstractPopulationVertex.non_pynn_default_parameters[
                     'ring_buffer_sigma']),
             incoming_spike_buffer_size=(
-                AbstractPopulationVertex.none_pynn_default_parameters[
+                AbstractPopulationVertex.non_pynn_default_parameters[
                     'incoming_spike_buffer_size']),
-            constraints=AbstractPopulationVertex.none_pynn_default_parameters[
+            constraints=AbstractPopulationVertex.non_pynn_default_parameters[
                 'constraints'],
-            label=AbstractPopulationVertex.none_pynn_default_parameters[
+            label=AbstractPopulationVertex.non_pynn_default_parameters[
                 'label'],
             v_init=MyModelCurrExpBase.initialize_parameters['v_init'],
             v_thresh=MyModelCurrExpBase.default_parameters['v_thresh'],
--- python_models8/model_data_holders/my_model_curr_my_synapse_type_data_holder.py
+++ python_models8/model_data_holders/my_model_curr_my_synapse_type_data_holder.py
@@ -9,17 +9,17 @@ from python_models8.neuron.builds.my_model_curr_my_synapse_type \
 class MyModelCurrMySynapseTypeDataHolder(DataHolder):
     def __init__(
             self, spikes_per_second=(
-                AbstractPopulationVertex.none_pynn_default_parameters[
+                AbstractPopulationVertex.non_pynn_default_parameters[
                     'spikes_per_second']),
             ring_buffer_sigma=(
-                AbstractPopulationVertex.none_pynn_default_parameters[
+                AbstractPopulationVertex.non_pynn_default_parameters[
                     'ring_buffer_sigma']),
             incoming_spike_buffer_size=(
-                AbstractPopulationVertex.none_pynn_default_parameters[
+                AbstractPopulationVertex.non_pynn_default_parameters[
                     'incoming_spike_buffer_size']),
-            constraints=AbstractPopulationVertex.none_pynn_default_parameters[
+            constraints=AbstractPopulationVertex.non_pynn_default_parameters[
                 'constraints'],
-            label=AbstractPopulationVertex.none_pynn_default_parameters[
+            label=AbstractPopulationVertex.non_pynn_default_parameters[
                 'label'],
             v_init=MyModelCurrMySynapseTypeBase.initialize_parameters[
                 'v_init'],
