15f498aa4b9373a01c87ad0a1d1216bc0db4ac47
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-10-10 13:53:36 +0100

    first integration test

---
+++ nmt_integration_tests/nwt_testbase.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2017-2019 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from p8_integration_tests.base_test_case import BaseTestCase
+from spynnaker8.utilities import neo_convertor
+
+
+class NwtTestBase(BaseTestCase):
+
+    def check_results(self, neo, expected_spikes):
+        spikes = neo_convertor.convert_spikes(neo)
+        v = neo_convertor.convert_data(neo, name="v")
+        print(spikes)
+        print(v)
+        for i, spike in enumerate(expected_spikes):
+            self.assertEqual(spikes[i][1], spike)
+        self.assertEqual(spikes.shape, (len(spikes), 2))
+        for spike in expected_spikes:
+            print(spike, v[spike][2], v[spike+1][2])
+            self.assertTrue(v[spike][2] > v[spike+1][2])
