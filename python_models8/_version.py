7976fc95086df862855573d5008fbcfddf682a2f
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-11-18 13:09:36 +0000

    update version date

--- python_models8/_version.py
+++ python_models8/_version.py
@@ -1,5 +1,5 @@
 __version__ = "1!5.1.1"
-__version_month__ = "August"
+__version_month__ = "November"
 __version_year__ = "2019"
-__version_day__ = "09"
+__version_day__ = "15"
 __version_name__ = "Liveware Problem"
