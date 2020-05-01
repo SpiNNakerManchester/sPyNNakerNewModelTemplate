4317a594c534432677c908b71cf658b7935d5ed9
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2020-05-01 11:54:47 +0100

    Fix for change of interface

--- c_models/src/my_models/plasticity/stdp/weight_dependence/my_weight_impl.c
+++ c_models/src/my_models/plasticity/stdp/weight_dependence/my_weight_impl.c
@@ -4,11 +4,10 @@
 plasticity_weight_region_data_t *plasticity_weight_region_data;
 
 address_t weight_initialise(
-        address_t address, uint32_t n_synapse_types,
-        uint32_t *ring_buffer_to_input_buffer_left_shifts) {
+        address_t address, uint32_t n_synapse_types, REAL *min_weights) {
 
     // This can be used to indicate the scaling used on the weights
-    use(ring_buffer_to_input_buffer_left_shifts);
+    use(min_weights);
 
     log_info("weight_initialise: starting");
     log_info("\tSTDP my weight dependence");
