6be415ac077d10167a304ae5faa04923ad275be0
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-02-08 11:08:31 +0000

    moved neo_convertor

--- nmt_integration_tests/nwt_testbase.py
+++ nmt_integration_tests/nwt_testbase.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from p8_integration_tests.base_test_case import BaseTestCase
-from spynnaker8.utilities import neo_convertor
+from spynnaker.utilities import neo_convertor
 
 
 class NwtTestBase(BaseTestCase):
