6723e8555dc9cb79f9f941f01bcf779cdbd735da
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-01-17 16:13:51 +0000

    fix add_path call

--- python_models8/__init__.py
+++ python_models8/__init__.py
@@ -3,5 +3,5 @@ from spynnaker.pyNN.data import SpynnakerDataView
 from python_models8 import model_binaries
 
 # This adds the model binaries path to the paths searched by sPyNNaker
-SpynnakerDataView.get_executable_finder().register_binary_search_path(
+SpynnakerDataView.get_executable_finder().add_path(
     os.path.dirname(model_binaries.__file__))
