46cecbcc0c5cbc9086805f3c0ae57e1fe616c221
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-04-12 12:03:00 +0100

    AbstractSpiNNakerCommon has been removed

--- python_models8/__init__.py
+++ python_models8/__init__.py
@@ -1,7 +1,7 @@
 import os
-from spynnaker.pyNN.abstract_spinnaker_common import AbstractSpiNNakerCommon
+from spynnaker.pyNN.spinnaker import SpiNNaker
 from python_models8 import model_binaries
 
 # This adds the model binaries path to the paths searched by sPyNNaker
-AbstractSpiNNakerCommon.register_binary_search_path(
+SpiNNaker.register_binary_search_path(
     os.path.dirname(model_binaries.__file__))
