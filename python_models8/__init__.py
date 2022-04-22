4802627e468f58d75fe55f118b4dba62719a0e9a
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-04-22 07:40:00 +0100

    executable_finder methods added to view

--- python_models8/__init__.py
+++ python_models8/__init__.py
@@ -3,5 +3,5 @@ from spynnaker.pyNN.data import SpynnakerDataView
 from python_models8 import model_binaries
 
 # This adds the model binaries path to the paths searched by sPyNNaker
-SpynnakerDataView.get_executable_finder().add_path(
+SpynnakerDataView.register_binary_search_path(
     os.path.dirname(model_binaries.__file__))
