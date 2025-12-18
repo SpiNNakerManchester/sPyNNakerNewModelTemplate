c88e7a614bd7dec6323368f86e67b7009ec4e403
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-12-18 12:27:03 +0000

    if $(SPYNNAKER_INSTALL_DIR)

--- c_models/makefiles/extra_neuron.mk
+++ c_models/makefiles/extra_neuron.mk
@@ -1,8 +1,5 @@
-ifndef SPYNNAKER_INSTALL_DIR:
-    CUR_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/)
-    # assume parallel clone
-    SPYNNAKER_INSTALL_DIR := $(abspath $(CUR_DIR)/../../../sPyNNaker/neural_modelling)
-endif
+CUR_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/)
+SPYNNAKER_INSTALL_DIR := $(strip $(if $(SPYNNAKER_INSTALL_DIR), $(SPYNNAKER_INSTALL_DIR), (abspath $(CUR_DIR)/../../../sPyNNaker/neural_modelling)))
 
 # Work out the top-level project folder
 MAKEFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
